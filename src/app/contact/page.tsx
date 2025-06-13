0d79f3fe320ef0131d3dca7ed661ffba1b8ebdf1
AsifAbbasi11996 <113164637+AsifAbbasi11996@users.noreply.github.com>
2025-06-13 15:15:31 +0530

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import { FormEvent, useState } from "react";
+
+function page() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Submitted:", { email, message });
+  };
+
+  return (
+    <div className="relative min-h-screen bg-black py-12 pt-30">
+      <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
+      <div className="max-w-2xl mx-auto p-4 relative z-10">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold mb-8">
+          Get in Touch !
+        </h1>
+        <p className="text-base text-teal-300 text-center my-2">
+          We're here to help with any questions about courses, programs, or
+          events. Reach out and let us know how we can assist you in your
+          musical journey
+        </p>
+        <form onSubmit={handleSubmit} className="space-y-4 mt-4">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder="Your Email Address"
+            className="p-4 text-white rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+          />
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            rows={5}
+            required
+            placeholder="Your Message"
+            className="p-4 text-white h-56 rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
+          />
+          <button
+            type="submit"
+            className="px-6 py-2 rounded-lg bg-teal-500 text-white font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default page;
